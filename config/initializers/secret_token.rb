e72b082672076c6d5405e4cf5eb026b391ef1032
Michael D. Ivey <ivey@gweezlebur.com>
2011-04-08 16:33:04 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colapp::Application.config.secret_token = '4ba1d51bdc42d171112180ed7a8a54be62a0cd9108cebd4637a6df7e0e4793db8f02f1f0a5fe101e4481d953f340c8b7e2d0c9110717a4e53aad919aef5c6008'
